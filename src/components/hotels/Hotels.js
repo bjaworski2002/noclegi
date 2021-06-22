7af83095932a7f23be09675ac734516b1b278392
bjaworski2002 <bjaworski2002@gmail.com>
2021-06-22 23:19:24 +0200

    Initial debug

--- src/components/hotels/Hotels.js
+++ src/components/hotels/Hotels.js
@@ -1,17 +1,18 @@
-import React, { Component } from "react"
+import React, {Component} from "react"
 import Hotel from "./hotel/Hotel"
 import styles from './Hotels.module.css'
-import PropTypes from 'prop-types'
+
 class Hotels extends Component {
-    render(){
-        return(
-        <div className={styles.cont}>
-            <p className={styles.title}>Oferty:</p>
-            <div className={styles.hotels}>
-                {this.props.hotels.map(hotel => <Hotel key={hotel.id} {...hotel}/>)}
+    render() {
+        return (
+            <div className={styles.cont}>
+                <p className={styles.title}>Oferty:</p>
+                <div className={styles.hotels}>
+                    {this.props.hotels.map(hotel => <Hotel key={hotel.id} {...hotel}/>)}
+                </div>
             </div>
-        </div>
         )
     }
 }
+
 export default Hotels
